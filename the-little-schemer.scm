15744892132efa582859d62f7c2fdfa9d01b305d
hackeryarn <artemchernyak@gmail.com>
2021-09-23 19:57:42 -0500

    The Third Commandment

--- the-little-schemer.scm
+++ the-little-schemer.scm
@@ -30,5 +30,63 @@
   (let loop ((lat lat) (seen '()))
     (cond
      ((null? lat) (quote ()))
-     ((eq? (car lat) a) (append (reverse seen) (cdr lat)))
+     ((eq? (car lat) a) (set-cdr! seen (cdr lat)))
      (else (loop (cdr lat) (cons (car lat) seen))))))
+
+(define (firsts l)
+  (cond
+   ((null? l) '())
+   (else (cons (car (car l))
+               (firsts (cdr l))))))
+
+(define (insertR new old lat)
+  (cond
+   ((null? lat) '())
+   ((eq? (car lat) old) (cons (car lat) (cons new (cdr lat))))
+   (else (cons (car lat) (insertR new old (cdr lat))))))
+
+(define (insertL new old lat)
+  (cond
+   ((null? lat) '())
+   ((eq? (car lat) old) (cons new lat))
+   (else (cons (car lat) (insertL new old (cdr lat))))))
+
+(define (subst new old lat)
+  (cond
+   ((null? lat) '())
+   ((eq? (car lat) old) (cons new (cdr lat)))
+   (else (cons (car lat) (subst new old (cdr lat))))))
+
+(define (subst2 new o1 o2 lat)
+  (cond
+   ((null? lat) '())
+   ((or (eq? (car lat) o1)
+        (eq? (car lat) o2))
+    (cons new (cdr lat)))
+   (else (cons (car lat) (subst2 new o1 o2 (cdr lat))))))
+
+(define (multirember a lat)
+  (cond
+   [(null? lat) '()]
+   [(eq? (car lat) a) (multirember a (cdr lat))]
+   [else (cons (car lat) (multirember a (cdr lat)))]))
+
+(define (multiinsertR new old lat)
+  (cond
+   [(null? lat) '()]
+   [(eq? (car lat) old)
+    (cons
+     (car lat)
+     (cons new (multiinsertR new old (cdr lat))))]
+   [else (cons (car lat)
+               (multiinsertR new old (cdr lat)))]))
+
+(define (multiinsertL new old lat)
+  (cond
+   [(null? lat) '()]
+   [(eq? (car lat) old)
+    (cons new
+          (cons old
+                (multiinsertL new old (cdr lat))))]
+   [else (cons (car lat)
+               (multiinsertL new old (cdr lat)))]))
